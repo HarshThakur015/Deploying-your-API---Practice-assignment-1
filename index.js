9b3f9fb6e9f1408853d61c8f7a36d1a847f7e339
HarshThakur015 <tsayamonly@gmail.com>
2025-02-06 10:58:02 +0530

    environment variables

--- index.js
+++ index.js
@@ -6,9 +6,25 @@ const port = 3010;
 
 app.use(express.static('static'));
 
+require('dotenv').config()
+
+const config = {
+  apiKey: process.env.API_KEY,
+  serverSecret: process.env.SERVER_SECRET,
+  isKalvian: process.env.IS_KALVIAN === 'true',
+};
+
+// console.log(config)
+
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
+  // res.send(config)
 });
+
+app.get('/env', (req, res) => {
+  res.send(config)
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
